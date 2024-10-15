0373fcae4efd5a58eb85c5a0ed6b6f08cc54e736
Andrei1144 <a06086206@gmail.com>
2024-10-15 17:23:12 -0300

    atualizado(17h-22min)

---
+++ src/main/java/com/skateflow/Skateflow/model/AdminLogin.java
@@ -0,0 +1,44 @@
+package com.skateflow.Skateflow.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class AdminLogin {
+
+    @Id
+    private Long id; // Use um identificador único
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "senha")
+    private String password; // Considere usar hashing para a senha
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
